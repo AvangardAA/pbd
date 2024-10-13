fe92126e2b88237e156fb2865aa1ffc4a7b2e18f
Artem Nazarenko <artem.n@peanut.trade>
2024-10-13 17:50:27 +0000

    non-repeat read added

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -166,10 +166,48 @@ def dlock():
         if connection2 and connection2.is_connected(): connection2.close()
 
 
+def read_no_repeat():
+    connection1 = create_connection()
+    connection2 = create_connection()
+
+    try:
+        cursor1 = connection1.cursor()
+        cursor2 = connection2.cursor()
+
+        print(f"Transaction 1 started: {datetime.now()}")
+        connection1.start_transaction(isolation_level='READ COMMITTED')
+        cursor1.execute("SELECT balance FROM accounts WHERE name = 'Alice'")
+        balance = cursor1.fetchone()[0]
+        print(f"Transaction 1 Alice balance: {balance}")
+
+        print(f"Transaction 2 started: {datetime.now()}")
+        connection2.start_transaction(isolation_level='READ COMMITTED')
+        cursor2.execute("UPDATE accounts SET balance = 1200 WHERE name = 'Alice'")
+        connection2.commit()
+
+        cursor1.execute("SELECT balance FROM accounts WHERE name = 'Alice'")
+        balance = cursor1.fetchone()[0]
+        print(f"Transaction 1 Alice balance: {balance}")
+
+        print(f"Transaction 1 commit(): {datetime.now()}")
+        connection1.commit()
+
+    except Error as e:
+        print(f"Error: {e}")
+        connection1.rollback()
+        connection2.rollback()
+    finally:
+        if cursor1: cursor1.close()
+        if connection1 and connection1.is_connected(): connection1.close()
+        if cursor2: cursor2.close()
+        if connection2 and connection2.is_connected(): connection2.close()
+
+
 if __name__ == "__main__":
     load_dotenv()
 
     read_uncommitted_demo()
     read_commit()
     read_repeat()
+    read_no_repeat()
     dlock()
